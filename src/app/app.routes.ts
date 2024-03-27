cbc150faa888910bb3701c4598026f3193740e2e
Noa Yacov <noayacov2023@gmail.com>
2024-03-27 13:14:36 +0200

    new changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { CategoriesListComponent } from './categories-list/categories-list.component';
 import { CategoryFormComponent } from './category-form/category-form.component';
+import { LearningModeComponent } from './learning-mode/learning-mode.component';
 
 export const routes: Routes = [
     {path: "", component: CategoriesListComponent},
     {path: "category/:id", component: CategoryFormComponent},
     {path: "newcategory", component: CategoryFormComponent},
+    {path: "learning-mode", component: LearningModeComponent},
 ];
