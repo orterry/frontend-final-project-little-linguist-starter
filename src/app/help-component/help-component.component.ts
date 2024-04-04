f2f485594a3c99bd792ab5a80eaa5056e7a68743
Or Terry <terry.or123456@gmail.com>
2024-04-05 00:29:46 +0300

    help

---
+++ src/app/help-component/help-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-help-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './help-component.component.html',
+  styleUrl: './help-component.component.css'
+})
+export class HelpComponentComponent {
+
+}
