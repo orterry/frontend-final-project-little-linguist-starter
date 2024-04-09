7621eafeae8db1f223f67afd401f48940c798e78
ELIAV <Eliavnektalov@gmail.com>
2024-04-09 15:55:30 +0300

    yas

--- src/app/services/game-chosse-dialog.service.ts
+++ src/app/services/game-chosse-dialog.service.ts
@@ -8,11 +8,11 @@ import { GameDifficulty } from '../../shared/model/game-difficulty.enum';
 export class GameChosseDialogService {
 
   private allGames : GameChooseDialog[] = [
-    new GameChooseDialog(1, "Wash Dishes", "Clean and dry all dishes", GameDifficulty.Medium),
-    new GameChooseDialog(2, "Vacuum Floors", "Remove dust and debris from floors",GameDifficulty .Medium),
-    new GameChooseDialog(3, "Laundry", "Wash, dry, and fold clothes",GameDifficulty .Hard),
-    new GameChooseDialog(4, "Cook Dinner", "Prepare a nutritious meal", GameDifficulty.Easy),
-    new GameChooseDialog(5, "Water Plants", "Give plants the necessary water",GameDifficulty .Easy),
+    new GameChooseDialog(1, "Word matching game", "Clean and dry all dishes", GameDifficulty.Medium),
+    new GameChooseDialog(2, "Word translation game", "Remove dust and debris from floors",GameDifficulty .Medium),
+    new GameChooseDialog(3, "Reverse word translation game", "Wash, dry, and fold clothes",GameDifficulty .Hard),
+    new GameChooseDialog(4, "Messy pun", "Prepare a nutritious meal", GameDifficulty.Easy),
+    new GameChooseDialog(5, "trivia game", "Give plants the necessary water",GameDifficulty .Easy),
     ];
 
 
