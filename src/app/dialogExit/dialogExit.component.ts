0651f8e3379470d328bf73559ee9869f4c9e134a
ELIAV <Eliavnektalov@gmail.com>
2024-05-09 18:08:15 +0300

    play

--- src/app/dialogExit/dialogExit.component.ts
+++ src/app/dialogExit/dialogExit.component.ts
@@ -1,12 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {  MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogRef } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-dialog-exit',
   standalone: true,
-  imports:[CommonModule],
+  imports:[CommonModule,MatButtonModule,MatIconModule,RouterLink,MatDialogContent,MatDialogClose,
+    MatDialogActions],
   templateUrl: './dialogExit.component.html',
-  styleUrls: ['./dialogExit.component.css']
+  styleUrl:'./dialogExit.component.css'
 })
 export class DialogExitComponent {
 C: any;
