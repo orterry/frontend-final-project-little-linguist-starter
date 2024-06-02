d0027518ac1e2e1167d7768fca36d856eb16ab84
Shem Bar <Shemb@flow-sec.com>
2024-06-02 20:03:46 +0300

    m

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,5 +7,5 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withComponentInputBinding()), provideAnimations(), provideFirebaseApp(() => initializeApp({"projectId":"little-c5d99","appId":"1:706426643919:web:19a4e13dcb4bdc760a5eba","storageBucket":"little-c5d99.appspot.com","apiKey":"AIzaSyBegoi8BUvrl0JRNB-_LmF6LhdxYzaltH0","authDomain":"little-c5d99.firebaseapp.com","messagingSenderId":"706426643919"})), provideFirestore(() => getFirestore())]
+  providers: [provideRouter(routes, withComponentInputBinding()), provideAnimations(), provideFirebaseApp(() => initializeApp({"projectId":"little-c5d99","appId":"1:706426643919:web:19a4e13dcb4bdc760a5eba","storageBucket":"little-c5d99.appspot.com","apiKey":"AIzaSyBegoi8BUvrl0JRNB-_LmF6LhdxYzaltH0","authDomain":"little-c5d99.firebaseapp.com","messagingSenderId":"706426643919"})), provideFirestore(() => getFirestore()), provideFirebaseApp(() => initializeApp({"projectId":"little-c5d99","appId":"1:706426643919:web:19a4e13dcb4bdc760a5eba","storageBucket":"little-c5d99.appspot.com","apiKey":"AIzaSyBegoi8BUvrl0JRNB-_LmF6LhdxYzaltH0","authDomain":"little-c5d99.firebaseapp.com","messagingSenderId":"706426643919"})), provideFirestore(() => getFirestore())]
 };
